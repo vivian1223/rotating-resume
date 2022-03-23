450981cbb5939a0985f79b0ffbd6052be3e3c8ba
Vivian Yeh <u4h93u6uhu@gmail.com>
2022-03-23 14:25:43 +0800

    feat: add resume intro layout & rwd

--- src/component/layout/Resume/index.js
+++ src/component/layout/Resume/index.js
@@ -3,20 +3,48 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import './index.scss';
 
+const iconListData = [
+    {
+        id:1,    
+        iconClass: "fa-brands fa-github",
+        link: 'https://github.com/vivian1223'
+    },
+    {
+        id:2,       
+        iconClass: "fa-brands fa-linkedin",
+        link: 'https://www.linkedin.com/in/vivian-yeh-36b21a1b7/'
+    },
+]
+
+export const IconList = ({list}) => {
+    return (
+        <ul className={classNames('icon-list')}>
+            {
+                list.map(({id ,iconClass, link})=> {
+                    return(
+                        <li key={id}><a href={link} target="_blank" rel="noreferrer"><FontAwesomeIcon className={classNames('icon')} icon={iconClass} /></a></li>
+                    )
+                })
+            }
+        </ul>
+    )
+}
+
 const Resume = () => {
     return(
-        <div className={classNames('self_intro')}>
-            <img src={require('./../../../images/selfie.jpg')} alt="Selfie" />
-            <h1>Vivian Yeh</h1>
-            <p>東吳大學 英文系 2015-2019</p>
-            <p>Hi！我是Vivian，是一名熱於學習的前端工程師，個性主動積極、擅長團隊協作及遠距溝通，熱愛研究新事物、閱讀與挑戰自我。</p>
-            <ul>
-                <li><a href="https://github.com/vivian1223" target="_blank" rel="noreferrer"><FontAwesomeIcon className={classNames('icon')} icon="fa-brands fa-github" /></a></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
+        <div className={classNames('resume')}>
+            <div className={classNames('self_intro')}>
+                <img src={require('./../../../images/selfie.jpg')} alt="Selfie" />
+                <h1>Vivian Yeh</h1>
+                <p>東吳大學 英文系 2015-2019</p>
+                <p>Hi！我是Vivian，是一名熱於學習的前端工程師，個性主動積極、擅長團隊協作及遠距溝通，熱愛研究新事物、閱讀與挑戰自我。</p>
+                <IconList list={iconListData}/>
+            </div>
+            <div className={classNames('skill')}>
+
+            </div>
         </div>
+        
     )
 }
 
