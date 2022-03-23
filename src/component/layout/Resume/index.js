61001e517a4e486e33ddeff1e7e22d9063c4267d
Vivian Yeh <u4h93u6uhu@gmail.com>
2022-03-23 17:55:39 +0800

    feat: SkillList component separation issue#5

--- src/component/layout/Resume/index.js
+++ src/component/layout/Resume/index.js
@@ -1,63 +1,11 @@
 import classNames from "classnames";
 import Intro from "./../../organisms/Intro";
 import { iconListData } from "./iconListData";
+import { SkillList } from "../../molecules/SkillList";
+import { skillListData } from "./skillListData";
 
 import './index.scss';
 
-
-const skillList =[
-    {
-        id:1,
-        title: '前端框架',
-        skills:['React、Redux', 'Vue 2、Vue 3', 'Vuex、Vue Router', 'TypeScript']
-    },
-    {
-        id:1,
-        title: '前端框架',
-        skills:['React、Redux', 'Vue 2、Vue 3', 'Vuex、Vue Router', 'TypeScript']
-    },
-    {
-        id:1,
-        title: '前端框架',
-        skills:['React、Redux', 'Vue 2、Vue 3', 'Vuex、Vue Router', 'TypeScript']
-    },
-    {
-        id:1,
-        title: '前端框架',
-        skills:['React、Redux', 'Vue 2、Vue 3', 'Vuex、Vue Router', 'TypeScript']
-    },
-    {
-        id:1,
-        title: '前端框架',
-        skills:['React、Redux', 'Vue 2、Vue 3', 'Vuex、Vue Router', 'TypeScript']
-    },
-    
-]
-
-export const SkillList = ({list}) => {
-    return (
-        <ul className={classNames('skill-list')}>
-            {
-                list.map(({id, title, skills})=>{
-                    return (
-                      <li key={id} className={classNames('skill-list-item')}>
-                        <h3>{title}</h3>
-                        <ul>
-                            {skills.map((item,index) => {
-                                return (
-                                 <li key={index}>
-                                    {item}
-                                </li>   
-                                ) 
-                            })}
-                        </ul>
-                    </li>  
-                    ) 
-                })
-            }
-        </ul>
-    )
-}
 
 const Resume = () => {
     return(
@@ -65,7 +13,7 @@ const Resume = () => {
             <Intro iconListData={iconListData} />
             <div className={classNames('skill')}>
                 <h2>前端開發技能</h2>
-                <SkillList list={skillList}/>
+                <SkillList list={skillListData}/>
             </div>
         </div>
         
---
+++ src/component/layout/Resume/skillListData.js
@@ -0,0 +1,28 @@
+export const skillListData =[
+    {
+        id:1,
+        title: '前端框架',
+        skills:['React、Redux', 'Vue 2、Vue 3', 'Vuex、Vue Router', 'TypeScript']
+    },
+    {
+        id:1,
+        title: '前端框架',
+        skills:['React、Redux', 'Vue 2、Vue 3', 'Vuex、Vue Router', 'TypeScript']
+    },
+    {
+        id:1,
+        title: '前端框架',
+        skills:['React、Redux', 'Vue 2、Vue 3', 'Vuex、Vue Router', 'TypeScript']
+    },
+    {
+        id:1,
+        title: '前端框架',
+        skills:['React、Redux', 'Vue 2、Vue 3', 'Vuex、Vue Router', 'TypeScript']
+    },
+    {
+        id:1,
+        title: '前端框架',
+        skills:['React、Redux', 'Vue 2、Vue 3', 'Vuex、Vue Router', 'TypeScript']
+    },
+    
+]
