73fd44bc60bfbe75ea8b85b9b8351ff863ddd240
Vivian Yeh <u4h93u6uhu@gmail.com>
2022-03-24 10:07:35 +0800

    feat: add horizontalLine Components

---
+++ src/component/atoms/HorizontalLine/index.js
@@ -0,0 +1,12 @@
+import classNames from "classnames";
+
+import './index.scss';
+
+const HorizontalLine = () => {
+    return(
+        <div className={classNames('horizontal-line')}></div>
+    )
+}
+
+export default HorizontalLine;
+
