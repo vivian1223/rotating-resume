8a6a166fbe1141d09790b294168e963e149d9cb4
Vivian Yeh <u4h93u6uhu@gmail.com>
2022-03-24 09:34:09 +0800

    feat: update skillList content

--- src/component/layout/Resume/skillListData.js
+++ src/component/layout/Resume/skillListData.js
@@ -5,24 +5,24 @@ export const skillListData =[
         skills:['React、Redux', 'Vue 2、Vue 3', 'Vuex、Vue Router', 'TypeScript']
     },
     {
-        id:1,
-        title: '前端框架',
-        skills:['React、Redux', 'Vue 2、Vue 3', 'Vuex、Vue Router', 'TypeScript']
+        id:2,
+        title: 'JavaScript',
+        skills:['ES Modules、元件模組化', 'Promise、async、await 處理 AJAX 網頁行為', 'RESTful API 串接']
     },
     {
-        id:1,
-        title: '前端框架',
-        skills:['React、Redux', 'Vue 2、Vue 3', 'Vuex、Vue Router', 'TypeScript']
+        id:3,
+        title: 'Layout',
+        skills:['HTML5、CSS3、RWD', 'SASS / SCSS / PUG', 'Tailwind、Bootstrap 5', 'element UI / element+']
     },
     {
-        id:1,
-        title: '前端框架',
-        skills:['React、Redux', 'Vue 2、Vue 3', 'Vuex、Vue Router', 'TypeScript']
+        id:4,
+        title: '團隊協作',
+        skills:['Git MR、PR', 'GitHub、GitLab', 'Trello、Jira', 'Scrum', 'Slack、Discord、Telegram']
     },
     {
-        id:1,
-        title: '前端框架',
-        skills:['React、Redux', 'Vue 2、Vue 3', 'Vuex、Vue Router', 'TypeScript']
+        id:5,
+        title: '延伸技能',
+        skills:['Postman', 'i18n', 'Debug', 'TOEIC 900、精通英文聽說讀寫', '遠端團隊協作']
     },
     
 ]
